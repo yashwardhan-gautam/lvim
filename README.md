7caa3c737dff895658cabf26feb5948c39fac89f
Yashwardhan Gautam <gautamyashwardhan@gmail.com>
2023-04-22 22:16:53 +0530

    update readme, codeforces

---
+++ README.md
@@ -0,0 +1,2 @@
+## LunarVim Config
+This project contains my configuration of [LunarVim](https://www.lunarvim.org/) editor.  
--- config.lua
+++ config.lua
@@ -84,12 +84,15 @@ lvim.builtin.treesitter.auto_install = true
 -- }
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
--- lvim.plugins = {
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "Igorjan94/codeforces.vim",
+  }
+}
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 -- vim.api.nvim_create_autocmd("FileType", {
